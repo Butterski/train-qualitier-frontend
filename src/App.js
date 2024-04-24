39e7ee54a0c071908c87fcfc5a8f739d156aa267
milosz.kucharski <miloszkucharski@gmail.com>
2024-04-24 21:32:36 +0200

    Refactor App.js to remove unused code and update routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import "./App.css";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import { Header } from "./Components/Header/Header";
 import { MainMenu } from "./Views/MainMenu/MainMenu";
-import { GraphView } from "./Views/GraphView/GraphView";
 import { MeasurementsList } from "./Views/MeasurementsList/MeasurementsList";
 import { MeasurementView } from "./Views/MeasurementView/MeasurementView";
 
 function App() {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const buttonName = searchParams.get("button");
+  // const location = useLocation();
+  // const searchParams = new URLSearchParams(location.search);
 
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<MainMenu />} />
-        <Route 
-          path="/graph" 
-          element={<GraphView buttonName={buttonName} />} 
-          />
         <Route
           path="/show-measurements"
-          element={<MeasurementsList buttonName={buttonName} />}
+          element={<MeasurementsList />}
         />
         <Route
-          path="/measurement-view"
-          element={<MeasurementView buttonName={buttonName} />}
+          path="/measurement-view/:optional_measurement_id?"
+          element={<MeasurementView />}
         />
       </Routes>
     </div>
