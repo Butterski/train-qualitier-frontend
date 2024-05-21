6936e41ec295a02cdd64035c665f92040333e9ed
milosz.kucharski <miloszkucharski@gmail.com>
2024-05-21 21:44:51 +0200

    Update Header and MeasurementData CSS styles

--- src/Views/MeasurementData/MeasurementData.js
+++ src/Views/MeasurementData/MeasurementData.js
@@ -11,49 +11,65 @@ import { BASE_URL } from "../../apiConfig";
 
 export const MeasurementData = () => {
   const [data, setData] = useState([]);
+  const transferData = (array) => {
+    let data = {
+      t: [],
+      gx: [],
+      gy: [],
+      gz: [],
+      ax: [],
+      ay: [],
+      az: [],
+      mx: [],
+      my: [],
+      mz: [],
+      temp: [],
+      hum: [],
+    };
+    for (let i = 0; i < array.length; i++) {
+      data["t"].push(array[i][0]);
+      data["gx"].push(array[i][1]);
+      data["gy"].push(array[i][2]);
+      data["gz"].push(array[i][3]);
+      data["ax"].push(array[i][4]);
+      data["ay"].push(array[i][5]);
+      data["az"].push(array[i][6]);
+      data["mx"].push(array[i][7]);
+      data["my"].push(array[i][8]);
+      data["mz"].push(array[i][9]);
+      data["temp"].push(array[i][10]);
+      data["hum"].push(array[i][11]);
+    }
+    return data;
+  };
   useEffect(() => {
-    fetch(`${BASE_URL}/get_mocked_measurement`)
+    const url = window.location.href;
+    const measureId = url.split("/").pop();
+    fetch(`${BASE_URL}/get_measurement_data/${measureId}`)
       .then((response) => response.json())
       .then((data) => {
-        setData(data);
+        setData(transferData(data));
       });
   }, []);
 
-  // useEffect(() => {
-  //   console.log(data);
-  // }, [data])
-
-  const ziparrays = (jsondata) => {
-    let keys = Object.keys(jsondata);
-    let output = [];
-    let length = jsondata[keys[0]].length;
-
-    for (let index = 0; index < length; index++) {
-      let obj = {};
-
-      keys.forEach((key) => {
-        obj[key] = jsondata[key][index];
-      });
-
-      output.push(obj);
-    }
-
-    return output;
-  };
+  
 
   return (
-    <main className="app-main">
+    <main className="app-main-data">
       <div className="data-grid">
         {data.length === 0 ? (
           <div>Loading...</div>
         ) : (
-          <>
-            <GyroscopeGraph data={ziparrays(data.gyro)} className="div1" />
-            <AccelerometerGraph data={ziparrays(data.acceleration)} className="div2" />
-            <MagnetometerGraph data={ziparrays(data.magnetometer)} className="div3" />
-            <TemperatureGraph data={data.temperature.x} className="div4" />
-            <WetnessGraph data={data.humidity.x} className="div5" />
-          </>
+          <div>
+            <GyroscopeGraph
+              data={data}
+              className="div1"
+            />
+            <AccelerometerGraph data={data} className="div2" />
+            <MagnetometerGraph data={data} className="div3" />
+            <TemperatureGraph data={data} className="div4" />
+            <WetnessGraph data={data} className="div5" />
+          </div>
         )}
       </div>
     </main>
